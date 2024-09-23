8a2ebcdf572131e665801787569b57aa5f4cfcdb
d1zzyroma <nataliawologin@gmail.com>
2024-09-23 14:22:43 +0200

    321

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -6,9 +6,13 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 const router = express.Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
+
 router.get('/contacts/:contactId', ctrlWrapper(getContactsByIdController));
+
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactByIdController));
+
 router.post('/contacts', ctrlWrapper(createContactController));
+
 router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
 
 export default router;
