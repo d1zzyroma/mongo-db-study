810409a7cd38ae4b28f3ac98ca77815b5413b428
d1zzyroma <nataliawologin@gmail.com>
2024-10-21 19:42:22 +0200

    321
    321

--- src/server.js
+++ src/server.js
@@ -7,27 +7,29 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middleware/swaggerDocs.js';
 
 const setupServer = () => {
   const app = express();
-  
+
   const logger = pino({
     transport: {
       target: 'pino-pretty',
     },
   });
+
   app.use('/uploads', express.static(UPLOAD_DIR));
   app.use(logger);
   app.use(cors());
   app.use(cookieParser());
-  
   app.use(express.json());
 
   app.use(router);
-  
+
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use('*', notFoundHandler);
-
   app.use(errorHandler);
 
   const PORT = process.env.PORT || 3000;
